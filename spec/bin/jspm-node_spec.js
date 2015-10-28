6f7a4408c3111729e781621ab6a3ad11c9bea99d
Vinson Chuong <vinsonchuong@gmail.com>
2015-10-27 20:21:21 -0700

    npm install also runs jspm install

--- spec/bin/jspm-node_spec.js
+++ spec/bin/jspm-node_spec.js
@@ -1,8 +1,60 @@
+import path from 'path';
 import {childProcess} from 'node-promise-es6';
+import * as fse from 'fs-extra-promise-es6';
+
+async function updateJson(filePath, overrides) {
+  const absolutePath = path.resolve(filePath);
+  const packageJson = await fse.readJson(absolutePath);
+  await fse.writeJson(absolutePath, Object.assign(packageJson, overrides));
+}
 
 describe('jspm-node', () => {
-  it('outputs "Hello World!"', async () => {
-    const {stdout} = await childProcess.exec('jspm-node');
-    expect(stdout.trim()).toBe('Hello World!');
+  describe('for a new project', () => {
+    beforeEach(async () => {
+      await fse.mkdirs('project');
+      await childProcess.exec('npm init -y', {cwd: 'project'});
+      await updateJson('project/package.json', {
+        scripts: {
+          prepublish: 'babel-node ../src/bin/jspm-node.js'
+        }
+      });
+    });
+
+    it('does not break npm install', async () => {
+      let successful;
+      try {
+        await childProcess.exec('npm install', {cwd: 'project'});
+        successful = true;
+      } catch (error) {
+        successful = false;
+      } finally {
+        expect(successful).toBe(true);
+      }
+    }, 60000);
+
+    describe('installing a package', () => {
+      beforeEach(async () => {
+        await updateJson('project/package.json', {
+          dependencies: {
+            'isomorphic-fetch': '*'
+          }
+        });
+      });
+
+      it('installs both the npm and the jspm version of the package', async () => {
+        await childProcess.exec('npm install', {cwd: 'project'});
+
+        expect(await fse.readJson(path.resolve('project/node_modules/isomorphic-fetch/package.json')))
+          .toEqual(jasmine.objectContaining({name: 'isomorphic-fetch'}));
+
+        const {stdout: version} = await childProcess.exec('npm show --json isomorphic-fetch version');
+        expect(await fse.readJson(path.resolve(`project/jspm_packages/npm/isomorphic-fetch@${JSON.parse(version)}/package.json`)))
+          .toEqual(jasmine.objectContaining({name: 'isomorphic-fetch'}));
+      }, 60000);
+    });
+
+    afterEach(async () => {
+      await fse.remove('project');
+    });
   });
 });
--- src/bin/jspm-node.js
+++ src/bin/jspm-node.js
@@ -1,10 +1,18 @@
+import * as fse from 'fs-extra-promise-es6';
+import jspm from 'jspm';
+import jspmCore from 'jspm/lib/core';
+
 async function run() {
-  await new Promise(() => {
-    process.stdout.write('Hello World!\n');
-  });
+  jspm.setPackagePath('.');
+  await jspmCore.init(null, false);
+
+  const packageJson = await fse.readJson('package.json');
+  for (const dependency of Object.keys(packageJson.dependencies || {})) {
+    await jspm.install(dependency, `npm:${dependency}`);
+  }
 }
 
 run().catch(error => {
-  process.stderr.write(error.stack + '\n');
+  process.stderr.write((error.stack || error) + '\n');
   process.exit(1);
 });
